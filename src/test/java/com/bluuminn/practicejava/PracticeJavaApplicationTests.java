0300d5ef1c19d7b5a51bf1af94a788a75c062aa6
Minju Kim <bluuminn@gmail.com>
2022-09-14 11:55:44 +0900

    Initial commit

---
+++ src/test/java/com/bluuminn/practicejava/PracticeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluuminn.practicejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

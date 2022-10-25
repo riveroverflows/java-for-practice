702e0fed842361ccd640d86170265737dcc5e597
Minju Kim <bluuminn@gmail.com>
2022-10-25 17:15:00 +0900

    테스트 코드 중복 제거
    - @BeforeEach 어노테이션을 이용하여 Calculator 인스턴스를 매 테스트마다 생성하는 이유는 첫번째 테스트 메소드를 실행할 때 Calculator 의 상태 값이 변경되어 다음 테스트 메소드에 영향을 미칠 수 있기 때문이다. 이와 같이 테스트 메소드 간에 영향을 미칠 경우 테스트 실행 순서나 Calculator 상태 값에 따라 테스트가 성공하거나 실패할 수 있다.

--- src/test/java/com/bluuminn/practicejava/jwpnextstep/CalculatorTest.java
+++ src/test/java/com/bluuminn/practicejava/jwpnextstep/CalculatorTest.java
@@ -1,34 +1,39 @@
 package com.bluuminn.practicejava.jwpnextstep;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     void add() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(6, 3);
         assertThat(result).isEqualTo(9);
     }
 
     @Test
     void subtract() {
-        Calculator calculator = new Calculator();
         int result = calculator.subtract(6, 3);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void multiply() {
-        Calculator calculator = new Calculator();
         int result = calculator.multiply(6, 3);
         assertThat(result).isEqualTo(18);
     }
 
     @Test
     void divide() {
-        Calculator calculator = new Calculator();
         int result = calculator.divide(6, 3);
         assertThat(result).isEqualTo(2);
     }

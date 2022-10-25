d993229aa42676181dc93874b7e55a7c4029cda3
Minju Kim <bluuminn@gmail.com>
2022-10-25 17:04:31 +0900

    테스트 코드로 값 검증 자동화

--- src/test/java/com/bluuminn/practicejava/jwpnextstep/CalculatorTest.java
+++ src/test/java/com/bluuminn/practicejava/jwpnextstep/CalculatorTest.java
@@ -2,28 +2,34 @@ package com.bluuminn.practicejava.jwpnextstep;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CalculatorTest {
     @Test
     void add() {
         Calculator calculator = new Calculator();
-        System.out.println("calculator.add(6, 3) = " + calculator.add(6, 3));
+        int result = calculator.add(6, 3);
+        assertThat(result).isEqualTo(9);
     }
 
     @Test
     void subtract() {
         Calculator calculator = new Calculator();
-        System.out.println("calculator.subtract(6, 3) = " + calculator.subtract(6, 3));
+        int result = calculator.subtract(6, 3);
+        assertThat(result).isEqualTo(3);
     }
 
     @Test
     void multiply() {
         Calculator calculator = new Calculator();
-        System.out.println("calculator.multiply(6, 3) = " + calculator.multiply(6, 3));
+        int result = calculator.multiply(6, 3);
+        assertThat(result).isEqualTo(18);
     }
 
     @Test
     void divide() {
         Calculator calculator = new Calculator();
-        System.out.println("calculator.divide(6, 3) = " + calculator.divide(6, 3));
+        int result = calculator.divide(6, 3);
+        assertThat(result).isEqualTo(2);
     }
 }

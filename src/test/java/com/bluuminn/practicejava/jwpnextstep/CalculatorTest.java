b5b130641a44b94e2c3b5b6273af7e5520379988
Minju Kim <bluuminn@gmail.com>
2022-10-25 17:24:11 +0900

    @BeforeEach, @AfterEach, @AfterAll 실행 순서 확인
    - @AfterAll 어노테이션은 테스트 클래스에 @TestInstance(TestInstance.Lifecycle.PER_CLASS) 어노테이션을 붙여줘야 한다.

--- src/test/java/com/bluuminn/practicejava/jwpnextstep/CalculatorTest.java
+++ src/test/java/com/bluuminn/practicejava/jwpnextstep/CalculatorTest.java
@@ -1,10 +1,10 @@
 package com.bluuminn.practicejava.jwpnextstep;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CalculatorTest {
 
     private Calculator calculator;
@@ -12,29 +12,44 @@ class CalculatorTest {
     @BeforeEach
     void setUp() {
         calculator = new Calculator();
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("after each");
+    }
+
+    @AfterAll
+    void teardownAll() {
+        System.out.println("after all");
     }
 
     @Test
     void add() {
         int result = calculator.add(6, 3);
         assertThat(result).isEqualTo(9);
+        System.out.println("add");
     }
 
     @Test
     void subtract() {
         int result = calculator.subtract(6, 3);
         assertThat(result).isEqualTo(3);
+        System.out.println("subtract");
     }
 
     @Test
     void multiply() {
         int result = calculator.multiply(6, 3);
         assertThat(result).isEqualTo(18);
+        System.out.println("multiply");
     }
 
     @Test
     void divide() {
         int result = calculator.divide(6, 3);
         assertThat(result).isEqualTo(2);
+        System.out.println("divide");
     }
 }

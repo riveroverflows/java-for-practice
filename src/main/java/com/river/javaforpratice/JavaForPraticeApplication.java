fce32dc9655211d9a3d2f989d760a397069db9b7
River <river@antigravity.kr>
2024-12-09 19:00:44 +0900

    Initial commit for new project

---
+++ src/main/java/com/river/javaforpratice/JavaForPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.river.javaforpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaForPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaForPraticeApplication.class, args);
+    }
+
+}

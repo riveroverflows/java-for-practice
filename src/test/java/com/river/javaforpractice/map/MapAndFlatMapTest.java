65d8c4564c5350efd10155b2ea04b492b44f80ab
River <river@antigravity.kr>
2024-12-09 19:47:16 +0900

    bluuminn 테스트 패키지 제거

--- src/test/java/com/river/javaforpractice/map/MapAndFlatMapTest.java
+++ src/test/java/com/river/javaforpractice/map/MapAndFlatMapTest.java
@@ -1,28 +1,30 @@
-package com.bluuminn.practicejava.mapandflatmap;
+package com.river.javaforpractice.map;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MapAndFlatMapTest {
+
     List<List<String>> list;
 
     @BeforeEach
     void setUp() {
+        System.out.println("setUp");
         list = Arrays.asList(
-                List.of("a"),
-                List.of("b"));
+            List.of("a"),
+            List.of("b"));
     }
 
     @Test
     void mapInOptional() {
         Optional<String> s = Optional.of("test");
+        System.out.println("s = " + s);
         assertEquals(Optional.of("TEST"), s.map(String::toUpperCase));
     }
 
@@ -30,29 +32,24 @@ public class MapAndFlatMapTest {
     void nestedOptionalString() {
         Optional<Optional<String>> nestedOptionalString1 = Optional.of(Optional.of("STRING"));
         Optional<Optional<String>> nestedOptionalString2 = Optional.of("string").map(s -> Optional.of("STRING"));
-        System.out.println(nestedOptionalString1);
-        System.out.println(nestedOptionalString2);
+        System.out.println("nestedOptionalString1 = " + nestedOptionalString1);
+        System.out.println("nestedOptionalString2 = " + nestedOptionalString2);
         assertEquals(nestedOptionalString1, nestedOptionalString2);
     }
 
     @Test
     void flatMapInOptional() {
         Optional<String> flatMapString = Optional.of("string").flatMap(s -> Optional.of("STRING"));
-        System.out.println(flatMapString);
+        System.out.println("flatMapString = " + flatMapString);
         assertEquals(Optional.of("STRING"), flatMapString);
     }
 
     @Test
-    void mapInStream() {
-        System.out.println(list);
-    }
-
-    @Test
     void flatMapInStream() {
-        List<String> flatMapList = list
-                .stream()
-                .flatMap(Collection::stream)
-                .toList();
-        System.out.println(flatMapList);
+        System.out.println("list = " + list);
+        List<String> flatMapList = list.stream()
+                                       .flatMap(Collection::stream)
+                                       .toList();
+        System.out.println("flatMapList = " + flatMapList);
     }
 }
